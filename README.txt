d14e6eb5525e43235cfdb369a382358f2f106a27
devKOB <vip_1id@hotmail.com>
2016-05-21 00:15:17 +0700

    dev findFares

---
+++ README.txt
@@ -0,0 +1,11 @@
+* Run Test
+mvn clean compile -Dspring.profiles.active=test-derby -Dtest=[class name of test] test
+
+* Run Jetty
+mvn clean compile -Dmaven.test.skip=true -Djetty.port=1515 jetty:run
+
+* RUN Deploy to tomcat HRMS_PY
+mvn clean compile package -Dmaven.test.skip=true tomcat7:redeploy -P deploy-ss-dev-derby-server
+
+*Run Create structure
+mvn clean compile -Dspring.profiles.active=create-structure-derby -Dmaven.test.skip=true -Djetty.port=8083 jetty:run -P create-derby-server
--- src/main/java/com/nbu/fares/controller/FaresController_Custom_Controller.java
+++ src/main/java/com/nbu/fares/controller/FaresController_Custom_Controller.java
@@ -1,12 +1,19 @@
 package com.nbu.fares.controller;
 
 import com.nbu.fares.service.RestService;
+import flexjson.JSONSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by tanaphatdev on 28/4/2559.
@@ -16,10 +23,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/service")
 public class FaresController_Custom_Controller {
 
-    protected static final Logger logger = LoggerFactory.getLogger(FaresController_Custom_Controller.class);
+    static Logger logger = LoggerFactory.getLogger(FaresController_Custom_Controller.class);
 
     @Autowired
     protected RestService restService;
 
+    @RequestMapping(value = "/findAllFares",method = RequestMethod.GET,headers = "Accept=application/json")
+        public ResponseEntity<String> findAllFares(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+    return restService.findAllFaresFromEngine();
+    }
+
+    @RequestMapping(value = "/findFares", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> findFares(@RequestParam(value = "faresCode",required = false)String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        ResponseEntity<String> result = null;
+        try{
+            logger.info("==============find by code================");
+            System.out.print("findFares controller"+code+"\n");
+            return   restService.findFaresFromEngine(code);
+        }catch (Exception e){
+            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
 
 }
